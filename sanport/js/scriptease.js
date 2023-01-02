ef6a8c0dfb8692133fb9b1956e6e75908f528bd4
Sandi Alfian <118152602+sandialfian21@users.noreply.github.com>
2023-01-02 22:53:45 +0700

    Menambahkan Effect Parallax

--- sanport/js/scriptease.js
+++ sanport/js/scriptease.js
@@ -10,11 +10,49 @@ $(document).ready(function(){
         // pindahkan scroll
         $('html, body').animate({
             scrollTop: elemenTujuan.offset().top - 57
-        },1250, 'easeOutBounce');
+        },1700, 'easeOutBounce');
 
         e.preventDefault();
     });
 });
+
+
+
+// Parallax
+// About
+$(window).on('load', function() {
+    $('.kiri').addClass('pMuncul')
+    $('.kanan').addClass('pMuncul')
+});
+
+$(window).scroll(function() {
+    var wScroll = $(this).scrollTop();
+    
+    // Jumbotron
+    $('.jumbotron img').css({
+        'transform' :'translate(0px , '+ wScroll/2.2 +'%)'
+    });
+    $('.jumbotron h1').css({
+        'transform' :'translate(0px , '+ wScroll/1.3 +'%)'
+    });
+    $('.jumbotron p').css({
+        'transform' :'translate(0px , '+ wScroll*1.5 +'%)'
+    });
+
+    // Portfolio
+    if(wScroll > $('.portfolio').offset().top -200 ) {
+        $('.portfolio .img-thumbnail').each(function(i) {
+            setTimeout(function() {
+                
+                $('.portfolio .img-thumbnail').eq(i).addClass('muncul');
+
+            }, 300 * (i+1));
+        });
+
+    }
+});
+
+
 
 
 
